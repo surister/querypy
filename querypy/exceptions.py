1764eb58fc9543b3b1e5f0b2258883bdd7408c33
surister <surister98@gmail.com>
2025-11-21 10:03:06 +0100

    Add basic exceptions

---
+++ querypy/exceptions.py
@@ -0,0 +1,7 @@
+class QueryEngineError(BaseException):
+    """Errors raised by the QueryEngine"""
+    pass
+
+class UnknownColumnError(QueryEngineError):
+    def __init__(self, col: str):
+        self.message = f"No column named {col!r}"
