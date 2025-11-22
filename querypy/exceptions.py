b8ccfd9d539a6ffa66dc47c38f8e74feb20b6840
surister <surister98@gmail.com>
2025-11-22 17:49:21 +0100

    Fix UnknownColumnError not showing the correct error message

--- querypy/exceptions.py
+++ querypy/exceptions.py
@@ -1,9 +1,8 @@
 class QueryEngineError(BaseException):
     """Errors raised by the QueryEngine"""
-
     pass
 
 
 class UnknownColumnError(QueryEngineError):
     def __init__(self, col: str):
-        self.message = f"No column named {col!r}"
+        super().__init__(f"No column named {col!r}")
