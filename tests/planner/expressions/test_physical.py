c183b6ed4bfdb264a2d1cfbeec6276b876dc4637
Ivan Sanchez <surister98@gmail.com>
2026-07-15 22:33:17 +0200

    Add tests for Max/Avg/Count aggregations

--- tests/planner/expressions/test_physical.py
+++ tests/planner/expressions/test_physical.py
@@ -1,6 +1,8 @@
 from unittest.mock import MagicMock
 
+from querypy.planner.dataframe import DataFrame
 from querypy.planner.expressions import PhysicalPlan
+
 from querypy.planner.expressions.physical import (
     Subtract,
     LiteralInteger,
@@ -8,10 +10,10 @@ from querypy.planner.expressions.physical import (
     Divide,
     Add,
     Alias,
-    Column,
+    Column, Aggregate, Max, Avg, Count
 )
 from querypy.planner.planner import create_physical_expr
-from querypy.planner.plans.physical import Projection, OrderBy
+from querypy.planner.plans.physical import Projection, OrderBy, HashAggregate
 from querypy.planner.expressions import logical
 from querypy.types_ import RecordBatch, Schema, Field, ArrowTypes, ColumnVector
 from tests import create_rb, create_logical_test_plan, create_physical_test_plan
@@ -86,3 +88,42 @@ def test_orderby():
     orderby.order_by[0][1] = True
     rb = list(orderby.execute())[0]
     assert rb.get_field(order_by_b) == ["a", "b", "c"]
+
+
+def test_aggregations():
+    a = [1, 2, 3, 4, 31, 2]
+    b = ["c", "b", "a", "a", "a", "c"]
+    data = [a, b]
+    dummy_plan = create_physical_test_plan(data)
+
+    # Max
+    aggr_result = HashAggregate(
+        dummy_plan,
+        group_expr=[Column(1)],
+        aggregate_expr=[Max(Column(0))],
+        schema=dummy_plan.schema()
+    ).execute()
+
+    assert (aggr_result[0].fields
+            == [['c', 'b', 'a'], [2, 2, 31]])
+    # Avg
+    aggr_result = HashAggregate(
+        dummy_plan,
+        group_expr=[Column(1)],
+        aggregate_expr=[Avg(Column(0))],
+        schema=dummy_plan.schema()
+    ).execute()
+
+    assert (aggr_result[0].fields
+            == [['c', 'b', 'a'], [1.5, 2.0, 12.666666666666666]])
+
+    # Count
+    aggr_result = HashAggregate(
+        dummy_plan,
+        group_expr=[Column(1)],
+        aggregate_expr=[Count(Column(0))],
+        schema=dummy_plan.schema()
+    ).execute()
+
+    assert (aggr_result[0].fields
+            == [['c', 'b', 'a'], [2, 1, 3]])
